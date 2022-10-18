9f38915e7758661bf2cfb72671dd549ec5ef79ae
RavishankarSPG <116069129+RavishankarSPG@users.noreply.github.com>
2022-10-18 15:46:40 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,35 @@
+package RavishankarGJava;
+
+public class Datatype {public static void main(String[] args) {
+//declaration of variables
+int intval;
+float fval;
+double dval;
+char cval;
+short sval;
+byte bval;
+long lval;
+boolean boolval;
+
+//Assign the values
+intval=562;
+
+System.out.println("Integer value=" +intval);
+fval=689.4f;
+System.out.println("Float value=" +fval);
+
+dval=789.5;
+cval='A';
+lval=6598;
+boolval=true;
+
+System.out.println("Double value=" +dval);
+System.out.println("Char value=" +cval);
+System.out.println("Long value=" +lval);
+System.out.println("Boolean value=" +boolval);
+
+}
+
+}
+
+	
