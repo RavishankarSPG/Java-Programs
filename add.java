f7263667f0ac7c2d89da7434fc9178ed3139f715
RavishankarSPG <116069129+RavishankarSPG@users.noreply.github.com>
2022-10-18 20:42:15 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,15 @@
+package RavishankarGJava;
+
+public class add {
+	public static void main(String[] args) {
+		int num1,num2,answer;
+		num1=45;
+		num2=68;
+		
+		//addition
+		answer=num1+num2;
+		System.out.println("The sum of "+num1+" and "+num2+" is "+answer);
+		
+	}
+
+}
