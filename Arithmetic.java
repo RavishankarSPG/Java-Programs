140056484711cabdc3e7a0dd39a838f8562483d1
RavishankarSPG <116069129+RavishankarSPG@users.noreply.github.com>
2022-10-18 18:07:25 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,30 @@
+package RavishankarGJava;
+
+public class Arithmetic {
+	
+	public static void main(String[] args) {
+		float num1,num2,answer;
+		num1=78.2f;
+		num2=53.4f;
+		
+		//addition
+		answer=num1+num2;
+		System.out.println("The sum of "+num1+" and "+num2+" is "+answer);
+		
+		//difference
+		answer=num1-num2;
+		System.out.println("The difference of "+num1+" and "+num2+" is "+answer);
+		
+		//product
+		answer=num1*num2;
+		System.out.println("The product of "+num1+" and "+num2+" is "+answer);
+		
+	    //quotient
+		answer=num1/num2;
+		System.out.println("The quotient of "+num1+" and "+num2+" is "+answer);
+		
+		//Remainder
+		answer=num1%num2;
+		System.out.println("The product of "+num1+" and "+num2+" is "+answer);}
+
+}
