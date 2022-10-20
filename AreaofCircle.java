b5f12618f8a80378d5cdcf56c28a971a35faeaea
RavishankarSPG <116069129+RavishankarSPG@users.noreply.github.com>
2022-10-20 11:11:51 +0530

    Add files via upload

---
+++ AreaofCircle.java
@@ -0,0 +1,14 @@
+package RavishankarGJava;
+
+public class AreaofCircle
+{
+	public static void main(String args[])
+	{
+		double pi,radius,area;
+		pi = 3.1415;
+		radius = 5;
+		area = pi*radius*radius;
+		System.out.println("Area of Circle is ="+area);
+
+}
+}
