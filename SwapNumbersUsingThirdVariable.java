1b16cbbf3b8f066529f0b893ea9c8f7667e6f19d
RavishankarSPG <116069129+RavishankarSPG@users.noreply.github.com>
2022-10-20 11:15:09 +0530

    Add files via upload

---
+++ SwapNumbersUsingThirdVariable.java
@@ -0,0 +1,18 @@
+package RavishankarGJava;
+
+public class SwapNumbersUsingThirdVariable
+{
+	public static void main(String[] args)
+	{
+		int a,b,temp;
+		a = 25;
+		b = 50;
+		System.out.println("Before interchanging a="+a+" and b="+b);
+		
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println("After interchanging a="+a+" and b="+b);
+		}
+
+	}
