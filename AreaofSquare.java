ad3c295f09cf81d6bdc63e45aadbd2da3ebcfe17
RavishankarSPG <116069129+RavishankarSPG@users.noreply.github.com>
2022-10-20 11:11:07 +0530

    Add files via upload

---
+++ AreaofSquare.java
@@ -0,0 +1,13 @@
+package RavishankarGJava;
+
+public class AreaofSquare 
+{
+	public static void main(String args[])
+	{
+		int s,area;
+		s = 8;
+		area = s*s;
+		System.out.println("Area of Square is ="+area);
+
+}
+}
