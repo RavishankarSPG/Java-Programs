ec8eac84aa4efe46b4df55e171f411fa27eee112
RavishankarSPG <116069129+RavishankarSPG@users.noreply.github.com>
2022-10-20 11:10:42 +0530

    Add files via upload

---
+++ AreaofRectangle.java
@@ -0,0 +1,14 @@
+package RavishankarGJava;
+
+public class AreaofRectangle
+{
+	public static void main(String args[])
+	{
+		int width,height,area;
+		width = 7;
+		height = 12;
+		area = width*height;
+		System.out.println("Area of Rectangle is ="+area);
+	}
+
+}
