4c198a9e45e7fd8faa6797d91c28a43e82d4dc9c
RavishankarSPG <116069129+RavishankarSPG@users.noreply.github.com>
2022-10-20 11:11:30 +0530

    Add files via upload

---
+++ AreaofTriangle.java
@@ -0,0 +1,14 @@
+package RavishankarGJava;
+
+public class AreaofTriangle
+{
+	public static void main(String args[])
+	{
+		int breadth,height,area;
+		breadth = 6;
+		height = 15;
+		area = (breadth*height)/2;
+		System.out.println("Area of Triangle is ="+area);
+
+}
+}
